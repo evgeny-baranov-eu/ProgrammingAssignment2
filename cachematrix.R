fc17c5c6d4bd846215fb4aba93fe41c05dc05edc
Eugene Baranov <eugene.baranov@dukascopy.com>
2015-10-21 14:06:32 +0300

    v1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+#####################################
+## Programming assigment #2        ##
+## Caching the Inverse of a Matrix ##
+#####################################
 
-## Write a short comment describing this function
+## cached matrix object constructor
+## x : original matrix
+## returns list with getters/setters
+##
+## USAGE:
+## x = makeCacheMatrix()
+## x$set(matrix(sample(1:100, 100, replace = T), nrow = 10, ncol = 10))
+## x$setSolve(solve(x$get()))
+## x$get()
+## x$getSolve()
 
 makeCacheMatrix <- function(x = matrix()) {
+  # init cache variable
+  c <- NULL
 
+  # create list with methods
+  list(
+    # original matrix setter
+    set = function(y) {
+      x <<- y
+      c <<- NULL
+    },
+    
+    # original matrix getter
+    get = function() return(x),
+    
+    # solve matrix getter
+    getSolve = function() return(c),
+    
+    # solve matrix setter
+    setSolve = function (s) c <<- s
+  )
 }
 
 
-## Write a short comment describing this function
-
+## test workaround for makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  } else {
+    message("calculate data")
+    m <- solve(x$get(), ...)
+    x$setSolve(m)
+    return(m)
+  }
 }
